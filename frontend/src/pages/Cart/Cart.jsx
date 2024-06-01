7b4a0099c4d74fa57ffa7aeabd9c444f5b314f5d
your_username <your_email@example.com>
2024-06-02 05:02:05 +0530

    corrected path for images in cart page

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -25,7 +25,7 @@ const Cart = () => {
             return (
               <div>
                 <div className="cart-items-title cart-items-item ">
-                  <img src={url + "/images/" + item.image} alt="" />
+                  <img src={url + "/uploads/" + item.image} alt="" />
                   <p>{item.name}</p>
                   <p>₹{item.price}</p>
                   <p>{cartItems[item._id]}</p>
