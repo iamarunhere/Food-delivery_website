94e8dd8b7b1fa18d10be79fc34e8435faae6e5a1
your_username <your_email@example.com>
2024-06-24 05:27:47 +0530

    added lazyload to image tag

--- frontend/src/components/FoodItems/FoodItems.jsx
+++ frontend/src/components/FoodItems/FoodItems.jsx
@@ -2,6 +2,8 @@ import React, { useContext } from "react";
 import "./FoodItems.css";
 import { assets } from "../../assets/assets";
 import { StoreContext } from "../../context/StoreContext";
+import LazyLoad from "react-lazyload";
+
 const FoodItems = ({ id, name, price, description, image }) => {
   const { cartItems, addToCart, removeFromCart, url } =
     useContext(StoreContext);
@@ -9,38 +11,40 @@ const FoodItems = ({ id, name, price, description, image }) => {
   return (
     <div className="food-items" id="food-items">
       <div className="food-items-image-container">
-        <img
-          className="food-items-image"
-          src={url + "/uploads/" + image}
-          alt=""
-        />
+        <LazyLoad height={200} offset={100} once>
+          <img
+            className="food-items-image"
+            src={url + "/uploads/" + image}
+            alt=""
+          />
+        </LazyLoad>
         {!cartItems[id] ? (
           <img
             className="add"
             onClick={() => addToCart(id)}
             src={assets.add_icon_white}
             alt=""
-          ></img>
+          />
         ) : (
           <div className="food-items-counter">
             <img
               onClick={() => removeFromCart(id)}
               src={assets.remove_icon_red}
               alt=""
-            ></img>
+            />
             <p>{cartItems[id]}</p>
             <img
               onClick={() => addToCart(id)}
               src={assets.add_icon_green}
               alt=""
-            ></img>
+            />
           </div>
         )}
       </div>
       <div className="food-items-info">
         <div className="food-items-name-rating">
           <p>{name}</p>
-          <img src={assets.rating_starts} alt=""></img>
+          <img src={assets.rating_starts} alt="" />
         </div>
         <p className="food-items-description">{description}</p>
         <p className="food-items-price">₹{price}</p>
